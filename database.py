922d411ddf61e1e7454b23ed93c82cb66813a3e1
abderrahmane <abderrahmaneMustapha030898@gmail.com>
2020-12-29 00:11:46 +0100

    feat :  fix db connection url

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://+psycopg2:@@localhost/algeria-ai"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres: @localhost/algeria-ai"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
